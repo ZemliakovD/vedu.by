5c31a9bd2ead139aebd55e1f160364faa77c3112
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-04-06 01:20:49 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,78 @@
 $('.category-slider').slick({
-    infinite: true,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left fa-beat" style="color: #008BF5;"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right fa-beat" style="color: #008BF5;"></i></button>',
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left fa-beat" style="color: #008BF5;"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right fa-beat" style="color: #008BF5;"></i></button>'
-});
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
 $('.clients-slider').slick({
+    dots: false,
     infinite: true,
+    speed: 2000,
+    prevArrow: '<button type="button" class="slick-clients-prev"></button>',
+    nextArrow: '<button type="button" class="slick-clients-next">></button>',
     slidesToShow: 3,
-    slidesToScroll: 1
-});
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
 
 $("#partfolio").click(function () { // ID откуда кливаем
@@ -33,6 +95,6 @@ $("#rates").click(function () { // ID откуда кливаем
 
 $("#footer").click(function () { // ID откуда кливаем
     $('html, body').animate({
-        scrollTop: $(".formwrap").offset().top // класс объекта к которому приезжаем
+        scrollTop: $(".footer").offset().top // класс объекта к которому приезжаем
     }, 1000); // Скорость прокрутки
 });
